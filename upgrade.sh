c3dbdc2cdafa3900e9fa19b56bace78c62dde98f
Fabian von Feilitzsch <fabian@fabianism.us>
2018-04-20 16:27:41 -0400

    add script for 3.7 to 3.9 update

---
+++ upgrade.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -ex
+
+TMPDIR=$(mktemp -d)
+trap "rm -rf $TMPDIR" EXIT
+
+git clone git@github.com:openshift/openshift-ansible ${TMPDIR}/openshift-ansible
+pushd ${TMPDIR}/openshift-ansible
+git checkout release-3.7
+popd
+
+cat <<-EOF > install-37.yml
+---
+- hosts: all
+  gather_facts: false
+  tasks:
+    - name: wait for host to come up
+      wait_for_connection:
+- import_playbook: ${TMPDIR}/openshift-ansible/playbooks/byo/config.yml
+EOF
+
+export PLAYBOOK='install-37.yml'
+vagrant up --no-parallel --provision
+
+pushd ${TMPDIR}/openshift-ansible
+git checkout release-3.9
+popd
+
+cat <<-EOF > upgrade-39.yml
+---
+- hosts: all
+  gather_facts: false
+  tasks:
+    - name: wait for host to come up
+      wait_for_connection:
+- import_playbook: ${TMPDIR}/openshift-ansible/playbooks/byo/openshift-cluster/upgrades/v3_9/upgrade.yml
+EOF
+
+export PLAYBOOK='upgrade-39.yml'
+vagrant provision --provision-with ansible
+
+rm -f install-37.yml
+rm -f upgrade-39.yml
